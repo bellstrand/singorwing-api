328563465645266218eac7109ed6c055ee056894
Bella <bellstrand@gmail.com>
2015-12-15 08:12:54 +0100

    artist more fields

--- src/models/artists.js
+++ src/models/artists.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose';
 let artists = new mongoose.Schema({
 	name: { type: String, required: true },
 	genre: String,
+	decade: String,
 	origin: String,
-	active: String,
-	image: String
+	image: String,
+	language: String,
+	members: String,
+	biography: String
 });
 
 export default mongoose.model('Artists', artists);
--- src/models/songs.js
+++ src/models/songs.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 let songs = new mongoose.Schema({
 	name: { type: String, required: true },
+	artist: { type: mongoose.Schema.Types.ObjectId, ref: "Artists" },
+	album: String,
 	released: String,
-	lyrics: String,
-	artist: { type: mongoose.Schema.Types.ObjectId, ref: "Artists" }
+	lyrics: String
 });
 
 export default mongoose.model('Songs', songs);
