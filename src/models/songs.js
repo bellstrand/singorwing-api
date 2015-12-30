f0592d3580116e7fcc74929f3f3494cb44778fc9
Bella <bellstrand@gmail.com>
2015-12-30 11:21:51 +0100

    distortions added to songs

--- src/models/songs.js
+++ src/models/songs.js
@@ -7,7 +7,12 @@ let songs = new mongoose.Schema({
 	album: { type: String, trim: true },
 	genre: { type: String, trim: true, required: true },
 	released: { type: Number, required: true },
-	lyrics: String
+	lyrics: String,
+
+	distortions: [{
+		distortion: { type: String, required: true, trim: true },
+		difficulty: { type: Number, required: true, min: 1, max: 10 }
+	}]
 });
 
 export default mongoose.model('Songs', songs);
